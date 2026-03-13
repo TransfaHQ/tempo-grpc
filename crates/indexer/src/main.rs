03b9500281036f4cb3a8d50a9b4f09e7fe329431
nelson <kamganelson@gmail.com>
2026-03-13 16:57:50 -0400

    fix: indexer backfill subscription

--- crates/indexer/src/main.rs
+++ crates/indexer/src/main.rs
@@ -6,16 +6,19 @@ use tonic::Request;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = BlockStreamClient::connect("https://grpc.moderato.trma.xyz").await?;
+    let mut client = BlockStreamClient::connect("https://grpc.moderato.trma.xyz")
+        .await?
+        .max_decoding_message_size(usize::MAX);
     let request = Request::new(BackfillRequest {
         from: 6000000,
-        to: 6_000_100,
+        to: 6_001_000,
+        size: 128,
     });
     let start = Instant::now();
     let response = client.backfill(request).await?;
     let mut stream = response.into_inner();
     while let Some(item) = stream.next().await {
-        println!("Received block: {}", item?.number);
+        println!("Received block: {}", item?.items.len());
     }
 
     println!("Done in {}", start.elapsed().as_millis());
